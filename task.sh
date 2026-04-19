98ab5b0ad2f0231c4cc91e3ce6f3355558b026f8
Vadym Vyshnevskyi <wallian169@gmail.com>
2026-04-19 14:54:39 +0300

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,16 @@
-#! /bin/bash
+# !/bin/bash
+
+BACKUP_DIR="/home/user/backups"
+mkdir -p "$BACKUP_DIR"
+
+if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file="$BACKUP_DIR/ShopDBReserve.sql"; then
+  echo "$(date): SUCCESS Production reserve copy created successfully"
+else
+  echo "$(date): ERROR Production reserve copying is failed!" >&2
+fi
+
+if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --no-create-info --result-file="$BACKUP_DIR/ShopDBDevelopment.sql"; then
+  echo "$(date): SUCCESS ShopDBDevelopment.sql was created."
+else
+  echo "$(date): ERROR Failed to transfer data to ShopDBDevelopment.sql" >&2
+fi
