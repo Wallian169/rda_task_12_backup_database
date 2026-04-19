56a29b143c70d9eff95e512538e85f689c4bb60d
Vadym Vyshnevskyi <wallian169@gmail.com>
2026-04-19 16:00:15 +0300

    fix: simplify solution

--- task.sh
+++ task.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file=ShopDBReserve.sql; then
-  echo "$(date): SUCCESS Production reserve copy created successfully"
-else
-  echo "$(date): ERROR Production reserve copying is failed!" >&2
-fi
-mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDBReserve.sql
-
-if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --no-create-info --result-file=ShopDBDevelopment.sql; then
-  echo "$(date): SUCCESS ShopDBDevelopment.sql was created."
-else
-  echo "$(date): ERROR Failed to transfer data to ShopDBDevelopment.sql" >&2
-fi
-mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < "ShopDBDevelopment.sql"
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment
