dc783bac8410062e4b83d9fa8fec2778daab31be
Vadym Vyshnevskyi <wallian169@gmail.com>
2026-04-19 15:17:41 +0300

    fix: add data extraction

--- task.sh
+++ task.sh
@@ -1,16 +1,17 @@
-# !/bin/bash
+#!/bin/bash
 
-BACKUP_DIR="/home/user/backups"
-mkdir -p "$BACKUP_DIR"
-
-if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file="$BACKUP_DIR/ShopDBReserve.sql"; then
+if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file=ShopDBReserve.sql; then
   echo "$(date): SUCCESS Production reserve copy created successfully"
 else
   echo "$(date): ERROR Production reserve copying is failed!" >&2
 fi
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBReserve;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDBReserve.sql
 
-if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --no-create-info --result-file="$BACKUP_DIR/ShopDBDevelopment.sql"; then
+if mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --no-create-info --result-file=ShopDBDevelopment.sql; then
   echo "$(date): SUCCESS ShopDBDevelopment.sql was created."
 else
   echo "$(date): ERROR Failed to transfer data to ShopDBDevelopment.sql" >&2
 fi
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBDevelopment;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < "ShopDBDevelopment.sql"
